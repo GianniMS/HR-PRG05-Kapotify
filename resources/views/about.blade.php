47ff00119fb770ce7cde256885cfd3c6c931e743
GianniMS <1008999@hr.nl>
2023-09-11 17:39:31 +0200

    Added 1st Blade template First Blade template with dynamic page title.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,17 +1,10 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
-<h1>About</h1>
-<a href="{{ route('home') }}">Home</a>
-<a href="{{ route('about') }}">About</a>
-<a href="{{ route('samples') }}">Samples</a>
-</body>
-</html>
+@extends ('layouts/web')
+
+@section('title', 'About')
+
+@section ('content')
+    <h1>About</h1>
+    <a href="{{ route('home') }}">Home</a>
+    <a href="{{ route('about') }}">About</a>
+    <a href="{{ route('samples') }}">Samples</a>
+@endsection
